3bc7dc5ba022ce24a07eade40b6bd51fb7c6571c
Arrio23 <148852047+Arrio23@users.noreply.github.com>
2023-10-27 06:48:50 +0700

    Add files via upload

---
+++ JavaReport/src/PBO6/Mahasiswa.java
@@ -0,0 +1,59 @@
+package PBO6;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RIO
+ */
+public class Mahasiswa {
+
+    private String nama;
+    private String nim;
+    private String alamat;
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the nim
+     */
+    public String getNim() {
+        return nim;
+    }
+
+    /**
+     * @param nim the nim to set
+     */
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
